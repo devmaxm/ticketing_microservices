37162a841b88e793fab52661022360a99cb8e4f3
Glipher G <max.maiokha.dev@gmail.com>
2023-10-09 11:29:57 +0300

    fix: split index.ts into app.ts and index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from 'express';
 import mongoose from 'mongoose'
-import {json} from 'body-parser'
-import errorHandlerMiddleware from "./middlewares/error-handler.middleware";
-import cookieSession from 'cookie-session'
-
-import {currentUserRouter} from "./routes/current-user.router";
-import {signInRouter} from "./routes/sign-in.router";
-import {signUpRouter} from "./routes/sign-up.router";
-import {signOutRouter} from "./routes/sign-out.router";
-import NotFoundError from "./errors/not-found.error";
-
-
-const app = express()
-
-// middlewares
-app.use(json())
-app.use(cookieSession({
-    keys: ['123']
-}))
-
-// routes
-app.use(currentUserRouter)
-app.use(signUpRouter)
-app.use(signInRouter)
-app.use(signOutRouter)
-
-app.all('*', async (req, res, next) => {
-    next(new NotFoundError())
-})
-
-// error handling
-app.use(errorHandlerMiddleware)
-
+import app from "./app";
 
 async function bootstrap() {
     try {
